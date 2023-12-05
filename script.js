a51296da3cd433cd978b530e81f539d28f616221
Kumar MM <public.kumar@gmail.com>
2023-12-04 18:02:37 -0800

    guess my number - continue

--- script.js
+++ script.js
@@ -10,16 +10,29 @@ document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
 */
 
-const x = function () {
-  console.log(23);
-};
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNumber;
+let score = 20;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
   if (!guess) {
     document.querySelector('.message').textContent = 'No number!';
+  } else {
+    if (guess === secretNumber) {
+      document.querySelector('.message').textContent = 'Correct Number!';
+    } else {
+      score--;
+      document.querySelector('.score').textContent = score;
+      if (score === 0) {
+        document.querySelector('.message').textContent = 'You lost the game';
+        document.querySelector('.check').disabled = true;
+      } else {
+        guess < secretNumber
+          ? (document.querySelector('.message').textContent = 'too low')
+          : (document.querySelector('.message').textContent = 'too high');
+      }
+    }
   }
-  console.log('dummy');
-  console.log('dummy2');
 });
